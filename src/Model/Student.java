a48e8dd4fe2a69a05b25507f17cbd6b8cc5ae5cb
mussumj <musarratjahanakhtar@gmail.com>
2019-01-28 21:49:43 +0530

    new code

---
+++ src/Model/Student.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Morris Mano
+ */
+public class Student {
+  public  String ID;
+ public    String Class;
+ public    String section;
+public     String name;
+ public    String fname;
+ public    String fnum;
+  public   String Aadhar;
+ public    String address;
+ public    String photo;
+     
+    
+    
+}
